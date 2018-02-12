f826c47df5de1211943f36b9064ea3189b6825f2
cjdango <cjdango@gmail.com>
2018-02-12 23:03:41 +0800

    Remove unnecessaries in login.ts

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 import { RegisterPage } from '../register/register';
 
-@IonicPage()
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',

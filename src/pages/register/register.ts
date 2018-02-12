8320b1265ced2b9f667156d8c867fed81d2ccf79
cjdango <cjdango@gmail.com>
2018-02-12 21:44:49 +0800

    Added login page UI
    *finish initial login page UI

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-register',
+  templateUrl: 'register.html'
 })
-export class HomePage {
+export class RegisterPage {
 
   constructor(public navCtrl: NavController) {
 

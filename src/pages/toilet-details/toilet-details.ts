053a48167114d4fbfab0211c92f074a4846fce08
cjdango <cjdango@gmail.com>
2018-05-19 21:05:37 +0800

    set running_man

--- src/pages/toilet-details/toilet-details.ts
+++ src/pages/toilet-details/toilet-details.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, ViewController } from 'ionic-angular';
+import { NavController, NavParams, ViewController, AlertController } from 'ionic-angular';
 
 import { ConvoPage } from '../convo/convo';
 import { ReviewsPage } from '../reviews/reviews';
 import { AuthProvider } from '../../providers/auth/auth';
 import { ToiletProvider } from '../../providers/toilet/toilet';
+
+import GeoFire from 'geofire';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Geolocation } from '@ionic-native/geolocation';
 
 /**
  * Generated class for the ToiletDetailsPage page.
@@ -26,12 +30,20 @@ export class ToiletDetailsPage {
 
   isReservedObservable: any;
 
+  posSubscriber: any;
+  posObservable: any;
+
+  geofire: GeoFire;
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public viewCtrl: ViewController,
     public authProvider: AuthProvider,
-    public toiletProvider: ToiletProvider
+    public toiletProvider: ToiletProvider,
+    public db: AngularFireDatabase,
+    public geolocation: Geolocation,
+    public alertCtrl: AlertController
   ) {
     this.toilet = navParams.get('hit').toilet;
     this.photoURL = navParams.get('hit').photoURL;
@@ -42,6 +54,8 @@ export class ToiletDetailsPage {
         this.isReserved = !!t.reserved_by;
         console.log('isReserved:', this.isReserved)
       });
+
+    this.geofire = new GeoFire(db.list('/running_men').query.ref);
   }
 
   ionViewWillLeave() {
@@ -54,6 +68,49 @@ export class ToiletDetailsPage {
     this.toiletProvider.updateToilet(toiletKey, {
       reserved_by: this.authProvider.currentUserId
     });
+
+    this.presentConfirm();
+  }
+
+  private presentConfirm() {
+    let alert = this.alertCtrl.create({
+      title: 'Share Distance?',
+      message: 'The host will be notified when you are 30m away from the toilet',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Share',
+          handler: () => {
+            console.log(this.navParams.get('hit'))
+
+            const toiletKey = this.navParams.get('hit').key;
+            this.posObservable = this.geolocation.watchPosition({ enableHighAccuracy: true });
+
+            this.posSubscriber = this.posObservable.subscribe(pos => {
+              this.geofire.set(toiletKey, [pos.coords.latitude, pos.coords.longitude]);
+            });
+
+            const geoQuery = this.geofire.query({
+              radius: .03,
+              center: this.navParams.get('hit').location
+            });
+            geoQuery.on('key_entered', (key, location, distance) => {
+              this.posSubscriber.unsubscribe();
+              this.geofire.remove(key);
+              geoQuery.cancel();
+
+            });
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 
   onConvoPage() {
